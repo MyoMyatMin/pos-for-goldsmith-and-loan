29b7138b9ed65ed50b81e1a4742c266beb7d378c
Myo Myat Min <myomyatmin315@gmail.com>
2024-06-23 17:52:00 +0700

    Fix some designs and remove unnecessary files

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -20,7 +20,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <link rel="stylesheet" href="/plugins/datatables-bs4/css/dataTables.bootstrap4.css">
 
 </head>
-<body class="hold-transition sidebar-mini">
+<body class="hold-transition sidebar-mini layout-fixed">
 <div class="wrapper">
 
   <!-- /.navbar -->
